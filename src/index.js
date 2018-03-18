39a68ee2f754f847f3494ab4620e86fc2f4c25f5
Abdelrahman Awad <logaretm1@gmail.com>
2018-03-19 01:52:27 +0200

    added esm index file

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import getColorModel from './getColorModel';
+import isAColor from './isAColor';
+import hexNumToDec from './hexNumToDec';
+import decNumToHex from './decNumToHex';
+import rgbToHex from './rgbToHex';
+import rgbToHsl from './rgbToHsl';
+import hexToRgb from './hexToRgb';
+import hslToRgb from './hslToRgb';
+import hexToHsl from './hexToHsl';
+import hslToHex from './hslToHex';
+import toRgb from './toRgb';
+import toHsl from './toHsl';
+import toHex from './toHex';
+import getRgbValues from './getRgbValues';
+import getHslValues from './getHslValues';
+import getHexValues from './getHexValues';
+import getRandomColor from './getRandomColor';
+import normalizeDecNum from './normalizeDecNum';
+import expandHexShorthand from './expandHexShorthand';
+
+export default {
+  getColorModel,
+  isAColor,
+  hexNumToDec,
+  decNumToHex,
+  rgbToHex,
+  rgbToHsl,
+  hexToRgb,
+  hslToRgb,
+  hexToHsl,
+  hslToHex,
+  toRgb,
+  toHex,
+  toHsl,
+  getRgbValues,
+  getHexValues,
+  getHslValues,
+  getRandomColor,
+  normalizeDecNum,
+  expandHexShorthand,
+  version: '__VERSION__'
+};

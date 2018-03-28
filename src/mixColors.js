32d7d7d96e2332e63f70bbd86f3c6c004b2d83ba
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-03-28 14:52:17 +0200

    mixColors remove floor color values

--- src/mixColors.js
+++ src/mixColors.js
@@ -5,9 +5,9 @@ import { mixValue } from './utils';
 export default function mixColors (color1, color2, ratio) {
   color1 = toRgb(color1);
   color2 = toRgb(color2);
-  const red = mixValue(color1.red, color2.red, ratio);
-  const green = mixValue(color1.green, color2.green, ratio);
-  const blue = mixValue(color1.blue, color2.blue, ratio);
+  const red = Math.floor(mixValue(color1.red, color2.red, ratio));
+  const green = Math.floor(mixValue(color1.green, color2.green, ratio));
+  const blue = Math.floor(mixValue(color1.blue, color2.blue, ratio));
   const alpha = mixValue(color1.alpha, color2.alpha, ratio);
   return new RgbColor({
     red,

5be2e9460978ce29bf2ba7587ddf8938ea6975b8
Abdelrahman Awad <logaretm1@gmail.com>
2019-03-26 13:32:05 +0200

    feat: allow HSL and HSV color models to accept decimals

--- src/parseHsl.ts
+++ src/parseHsl.ts
@@ -1,15 +1,28 @@
 import { IHslColor } from './types/hsl';
 
-export function parseHsl (value: string | null): IHslColor | null {
+export interface HslParsingOptions {
+  allowDecimal: boolean;
+}
+
+const defaultOpts: HslParsingOptions = {
+  allowDecimal: true
+};
+
+export function parseHsl (value: string | null, options: HslParsingOptions = defaultOpts): IHslColor | null {
   if (typeof value !== 'string') {
     return null;
+  }
+
+  let regex = /^hsla?\(\s*(\d+(?:\.\d+)?)\s*,\s*(\d+(?:\.\d+)?)%\s*,\s*(\d+(?:\.\d+)?)%\s*,*\s*(\d*(?:\.\d+)*)*\)/i;
+  if (!options.allowDecimal) {
+    regex = /^hsla?\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*,*\s*(\d*(?:\.\d+)*)*\)/i;
   }
 
   // will consider hsl/hsla color prefix as a valid input color
   // while the output will be a valid web colors
   // valid input colors examples 'hsl(255, 100%, 50%, 0.5)', 'hsla(100, 100%, 50%)'
   // the output for the inputted examples 'hsla(255, 100%, 50%, 0.5)', 'hsl(100, 100%, 50%)'
-  const match = value.match(/^hsla?\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*,*\s*(\d*(?:\.\d+)*)*\)/i);
+  const match = value.match(regex);
   if (!match || match.length < 4) {
     return null;
   }
--- src/parseHsv.ts
+++ src/parseHsv.ts
@@ -1,15 +1,28 @@
 import { IHsvColor } from './types/hsv';
 
-export function parseHsv (value: IHsvColor | string | any): IHsvColor | null {
+export interface HsvParsingOptions {
+  allowDecimal: boolean;
+}
+
+const defaultOpts: HsvParsingOptions = {
+  allowDecimal: true
+};
+
+export function parseHsv(value: IHsvColor | string, options: HsvParsingOptions = defaultOpts): IHsvColor | null {
   if (typeof value !== 'string') {
     return null;
+  }
+
+  let regex = /^hsva?\(\s*(\d+(?:\.\d+)?)\s*,\s*(\d+(?:\.\d+)?)%\s*,\s*(\d+(?:\.\d+)?)%\s*,*\s*(\d*(?:\.\d+)*)*\)/i;
+  if (!options.allowDecimal) {
+    regex = /^hsva?\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*,*\s*(\d*(?:\.\d+)*)*\)/i;
   }
 
   // will consider hsv/hsva color prefix as a valid input color
   // while the output will be a valid web colors
   // valid input colors examples 'hsv(255, 100%, 50%, 0.5)', 'hsva(100, 100%, 50%)'
   // the output for the inputted examples 'hsva(255, 100%, 50%, 0.5)', 'hsv(100, 100%, 50%)'
-  const match = value.match(/^hsva?\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*,*\s*(\d*(?:\.\d+)*)*\)/i);
+  const match = value.match(regex);
   if (!match || match.length < 4) {
     return null;
   }
